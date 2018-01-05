823bd394c5d4d71786456a002b2c0d8c17718bb2
Renaud Paquay <rpaquay@chromium.org>
2018-01-04 16:32:20 -0800

    Move listview column header on top
    This is done by making the listview a user control that is embedded
    twice per library. Various parts are made invisible depending on context.

---
+++ UserInterface/SteamLibraryListViewUserControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using SteamLibraryExplorer.ViewModel;
+
+namespace SteamLibraryExplorer.UserInterface {
+  /// <summary>
+  /// Interaction logic for SteamLibraryListViewUserControl.xaml
+  /// </summary>
+  public partial class SteamLibraryListViewUserControl {
+    public SteamLibraryListViewUserControl() {
+      InitializeComponent();
+      Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs routedEventArgs) {
+      ViewModel.ListView = ListView;
+    }
+
+    public SteamLibraryViewModel ViewModel => (SteamLibraryViewModel)DataContext;
+
+    public bool DisableMouseWheelScrolling { get; set; }
+
+    public bool HideListViewColumnHeaders { get; set; }
+
+    public bool HideListViewContent { get; set; }
+
+    private void ListViewColumnHeader_Click(object sender, RoutedEventArgs e) {
+      ViewModel.OnGamesListViewColumnsHeaderClick(new ListViewColumnClickEventArgs(ListView, (GridViewColumnHeader)sender));
+    }
+
+    private void SteamGamesCollectionViewSource_OnFilter(object sender, FilterEventArgs e) {
+      ViewModel.OnFilterGameEntry(e);
+    }
+
+    private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
+      if (DisableMouseWheelScrolling) {
+        if (sender is ListView && !e.Handled) {
+          e.Handled = true;
+          var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+          eventArg.RoutedEvent = MouseWheelEvent;
+          eventArg.Source = sender;
+          ScrollViewer topMostViewer = null;
+          for (var parent = ((FrameworkElement)sender).Parent; parent != null;) {
+            if (parent is ScrollViewer) {
+              topMostViewer = (ScrollViewer)parent;
+            }
+
+            if (parent is FrameworkElement) {
+              parent = VisualTreeHelper.GetParent(parent);
+            } else {
+              parent = null;
+            }
+          }
+          topMostViewer?.RaiseEvent(eventArg);
+        }
+      }
+    }
+  }
+}
--- UserInterface/SteamLibraryUserControl.xaml.cs
+++ UserInterface/SteamLibraryUserControl.xaml.cs
@@ -1,59 +1,10 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
-using SteamLibraryExplorer.ViewModel;
-
-namespace SteamLibraryExplorer.UserInterface {
+﻿namespace SteamLibraryExplorer.UserInterface {
   /// <summary>
   /// Interaction logic for SteamLibraryUserControl.xaml
   /// </summary>
   public partial class SteamLibraryUserControl {
     public SteamLibraryUserControl() {
       InitializeComponent();
-      Loaded += OnLoaded;
-    }
-
-    private void OnLoaded(object sender, RoutedEventArgs routedEventArgs) {
-      ViewModel.ListView = ListView;
-    }
-
-    public bool DisableMouseWheelScrolling { get; set; }
-
-    public SteamLibraryViewModel ViewModel => (SteamLibraryViewModel)DataContext;
-
-    private void ListViewColumnHeader_Click(object sender, RoutedEventArgs e) {
-      ViewModel.OnGamesListViewColumnsHeaderClick(new ListViewColumnClickEventArgs(ListView, (GridViewColumnHeader)sender));
-    }
-
-    private void SteamGamesCollectionViewSource_OnFilter(object sender, FilterEventArgs e) {
-      ViewModel.OnFilterGameEntry(e);
-    }
-
-    private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
-      if (DisableMouseWheelScrolling) {
-        if (sender is ListView && !e.Handled) {
-          e.Handled = true;
-          var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
-          eventArg.RoutedEvent = MouseWheelEvent;
-          eventArg.Source = sender;
-          ScrollViewer topMostViewer = null;
-          for (var parent = ((FrameworkElement)sender).Parent; parent != null; ) {
-            if (parent is ScrollViewer) {
-              topMostViewer = (ScrollViewer)parent;
-            }
-
-            if (parent is FrameworkElement) {
-              parent = VisualTreeHelper.GetParent(parent);
-            }
-            else {
-              parent = null;
-            }
-          }
-          topMostViewer?.RaiseEvent(eventArg);
-        }
-      }
     }
   }
 }
